51b189f6b824df9d36a4ce2897eb8761d1bdc95d
Jnt10 <janetchen1011@gmail.com>
2023-12-19 09:57:46 +0800

    week15

---
+++ week15/week15-1a.py
@@ -0,0 +1,8 @@
+//把最大的2個相乘 - 最小的2個相乘
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        nums.sort()
+        
+        N = len(nums)
+        
+        return nums[N-1]*nums[N-2] - nums[0]*nums[1]
